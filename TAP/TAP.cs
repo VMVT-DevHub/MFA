a2d06c728234bd774e394234eaa0f82c12220a40
AntanasVMVT <antanas.jakubenas@vmvt.lt>
2024-05-15 11:14:29 +0300

    username email parsing

--- TAP/TAP.cs
+++ TAP/TAP.cs
@@ -16,36 +16,45 @@ if (!Directory.Exists(pth)) { Directory.CreateDirectory(pth); }
 
 
 app.MapPost("/login", async (HttpContext ctx, LoginRequest cred) => {
-	try {
-		using var conn = new LdapConnection();
-		conn.Connect(cfg.ADHost, cfg.ADPort);
-		conn.Bind($"{cfg.ADDomain}\\{cred.UserName}", cred.UserPass);
+	if(cred.UserName?.Length>3 && cred.UserPass?.Length>3) {
+		try {
 
-		var req = new Graph.Request(cfg.Tenant,cfg.ClientId,cfg.ClientSecret);
-		var grp = new Graph.Requests.TemporaryAccessPass(req){ IsUsableOnce=cfg.PassOnetime, LifetimeInMinutes=cfg.PassLifetime };
-		
-		var rsp = await grp.Process($"{cred.UserName}@{cfg.Domain}");
-		var ret = new LoginResponse();
+			var usr = cred.UserName.Split("@")[0];
+			using var conn = new LdapConnection();
+			conn.Connect(cfg.ADHost, cfg.ADPort);
+			conn.Bind($"{cfg.ADDomain}\\{usr}", cred.UserPass);
 
-		if(rsp.Error is not null){
-			ctx.Response.StatusCode=400;
-			ret.Error = rsp.Error.Message;
-			Log.Write(cred.UserName??"*null*",JsonSerializer.Serialize(rsp.Error),ctx);
-		} else {
-			ret.AccessPass = rsp.AccessPass;
-			ret.CreatedTime = rsp.CreatedDateTime;
-			ret.LifeTime = rsp.LifetimeInMinutes;
-			Log.Write(cred.UserName??"*null*","Ok",ctx);
+			var req = new Graph.Request(cfg.Tenant,cfg.ClientId,cfg.ClientSecret);
+			var grp = new Graph.Requests.TemporaryAccessPass(req){ IsUsableOnce=cfg.PassOnetime, LifetimeInMinutes=cfg.PassLifetime };
+			
+			var rsp = await grp.Process($"{usr}@{cfg.Domain}");
+			var ret = new LoginResponse();
+
+			if(rsp.Error is not null){
+				ctx.Response.StatusCode=400;
+				ret.Error = rsp.Error.Message;
+				Log.Write(usr,JsonSerializer.Serialize(rsp.Error),ctx);
+			} else {
+				ret.AccessPass = rsp.AccessPass;
+				ret.CreatedTime = rsp.CreatedDateTime;
+				ret.LifeTime = rsp.LifetimeInMinutes;
+				Log.Write(usr,"Ok",ctx);
+			}
+
+			await ctx.Response.WriteAsJsonAsync(ret);
 		}
-
-		await ctx.Response.WriteAsJsonAsync(ret);
-	}
-	catch (LdapException ex) {
-		var err= ex.ResultCode==49?"Neteisingi prisijungimo duomenys": $"{ex.Message} ({ex.ResultCode})";
-		ctx.Response.StatusCode = 401;
-		
+		catch (LdapException ex) {
+			var err= ex.ResultCode==49?"Neteisingi prisijungimo duomenys": $"{ex.Message} ({ex.ResultCode})";
+			ctx.Response.StatusCode = 401;
+			
+			Log.Write(cred.UserName??"*null*",err,ctx);
+			Thread.Sleep(2000);
+			await ctx.Response.WriteAsJsonAsync(new LoginResponse(){Error=err});
+		}
+	} else {
+		var err= "Neteisingi prisijungimo duomenys";
+		ctx.Response.StatusCode = 401;		
 		Log.Write(cred.UserName??"*null*",err,ctx);
-		Thread.Sleep(2000);
 		await ctx.Response.WriteAsJsonAsync(new LoginResponse(){Error=err});
 	}
 });
